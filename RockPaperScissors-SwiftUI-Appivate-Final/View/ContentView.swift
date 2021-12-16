8286973a7e1954dc7069478bb5f40fb9e2bc4cfb
gary.maccabe <gary.maccabe@aix.dev>
2021-12-16 19:20:43 +0000

    Add gameModel to contentView to allow player selection

--- RockPaperScissors-SwiftUI-Appivate-Final/View/ContentView.swift
+++ RockPaperScissors-SwiftUI-Appivate-Final/View/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var result = ""
+    private let gameModel = GameModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack.init(spacing: 30) {
+            Text("\(result)")
+                .font(.largeTitle)
+            Button {
+                result = gameModel.getGameResult(playerChoice: "rock", computerChoice: gameModel.setComputersChoice())
+            } label: {
+                Label("Rock", systemImage: "cube.fill")
+            }
+            Button {
+                result = gameModel.getGameResult(playerChoice: "paper", computerChoice: gameModel.setComputersChoice())
+            } label: {
+                Label("Paper", systemImage: "book")
+            }
+            Button {
+                result = gameModel.getGameResult(playerChoice: "scissors", computerChoice: gameModel.setComputersChoice())
+            } label: {
+                Label("Scissors", systemImage: "scissors")
+            }
+        }
     }
 }
 
